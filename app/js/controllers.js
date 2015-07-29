cae8e56eca68994ef74e0d57f3eb12e545d7f1e3
Rohan Seth <rseth2@illinois.edu>
2015-07-29 07:40:51 -0500

    Angular tutorial

---
+++ app/js/controllers.js
@@ -0,0 +1,13 @@
+	'use strict';
+
+/* Controllers */
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('phones/phones.json').success(function(data) {
+    $scope.phones = data;
+  });
+
+  $scope.orderProp = 'age';
+}]);
